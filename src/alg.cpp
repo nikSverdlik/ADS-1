85e04fb2b08deb8e5529d0e1d03cd5a77b7db40a
nikSverdlik <niksverdlik@gmail.com>
2026-02-25 20:28:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,9 +4,9 @@
 
 
 bool checkPrime(uint64_t value) {
-  if (value<2) return false;
-  for (uint64_t i = 2; i<=value; i++){
-      if (value%i == 0 && i!=value){
+  if (value < 2) return false;
+  for (uint64_t i = 2; i <= value; i++) {
+      if (value%i == 0 && i != value) {
           return false;
       }
   }
@@ -16,10 +16,10 @@ bool checkPrime(uint64_t value) {
 uint64_t nPrime(uint64_t n) {
   uint64_t numOfPrime = 0;
   uint64_t chekChislo = 1;
-  if (n<1) return 0;
-  while (numOfPrime < n){
+  if (n < 1) return 0;
+  while (numOfPrime < n) {
       chekChislo++;
-      if (checkPrime(chekChislo)){
+      if (checkPrime(chekChislo)) {
           numOfPrime++;
       }
   }
@@ -27,8 +27,8 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  uint64_t nextChislo = value+1;
-  while (!checkPrime(nextChislo)){
+  uint64_t nextChislo = value + 1;
+  while (!checkPrime(nextChislo)) {
       nextChislo++;
   }
   return nextChislo;
@@ -36,9 +36,9 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t primeSum = 0;
-  for (uint64_t i = 2; i < hbound; i++){
-      if (checkPrime(i)){
-          primeSum+=i;
+  for (uint64_t i = 2; i < hbound; i++) {
+      if (checkPrime(i)) {
+          primeSum += i;
       }
   }
   return primeSum;
@@ -46,8 +46,8 @@ uint64_t sumPrime(uint64_t hbound) {
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t couples = 0;
-  for (uint64_t i = lbound; i+2 < hbound; i++){
-      if (checkPrime(i) && checkPrime(i+2)){
+  for (uint64_t i = lbound; i + 2 < hbound; i++) {
+      if (checkPrime(i) && checkPrime(i + 2)) {
           couples++;
       }
   }
